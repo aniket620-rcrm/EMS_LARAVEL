6faaa4bcae8fe3fb9d33d7aef7341722038cd67a
fillwithgreencolor <aniket620aniket@gmail.com>
2023-04-14 09:43:50 +0530

    Added api for leave and employee

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +27,11 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('/register',[AuthenticationController::class , 'register']);
-Route::get('/login',[AuthenticationController::class , 'login']);
+// Route::get('/login',[AuthenticationController::class , 'login']);
 Route::post('/login',[AuthenticationController::class , 'login']);
+
+Route::apiResource('/users',UserController::class);
+Route::apiResource('/activeLeaves',LeaveController::class);
 
 // Route::post('/send-email', [MailController::class , 'mail']);
 
