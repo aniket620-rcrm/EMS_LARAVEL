0b1b5011fc0404a856ed3e01e51141b8e8878989
Amanpati Tripathi <tripathiaman777@gmail.com>
2023-04-18 16:27:58 +0530

    go

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Leave;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class LeaveController extends Controller
@@ -164,30 +163,32 @@ class LeaveController extends Controller
     }
 
 public function leaveRequest(Request $request)
+
 {
-   // Validate the form input
-   $validatedData = $request->validate([
-    'user_id' => 'required|integer',
-    'leave_start_date' => 'required|date',
-    'leave_end_date' => 'required|date|after_or_equal:leave_start_date',
-    'approved_by' => 'required'
-]);
+    // Validate the form input
+    $validatedData = $request->validate([
+        'user_id' => 'required|integer',
+        'leave_start_date' => 'required|date',
+        'leave_end_date' => 'required|date|after_or_equal:leave_start_date',
+        
+    ]);
+    
+    // // Create a new leave record with the validated data
+    $leave = new Leave();
+    $leave->user_id = $validatedData['user_id'];
+    $leave->leave_start_date = $validatedData['leave_start_date'];
+    $leave->leave_end_date = $validatedData['leave_end_date'];
+    $leave->approval_status = 2;
+    $leave->approved_by = 'Pending';
+    $leave->created_at = now();
+    $leave->updated_at = now();
+    $leave->save();
+    return $leave;
+    
+// // Save the leave record to the database
 
-// Create a new leave record with the validated data
-$leave = new Leave();
-$leave->user_id = $validatedData['user_id'];
-$leave->leave_start_date = Carbon::createFromFormat('d-m-Y', $validatedData['start']);
-$leave->leave_end_date = Carbon::createFromFormat('d-m-Y', $validatedData['end']);
-$leave->approval_status = 0;
-$leave->approved_by = $validatedData['approved_by'];
-$leave->created_at = now();
-$leave->updated_at = now();
-
-// Save the leave record to the database
-$leave->save();
-
-// Redirect back to the form with a success message
-return redirect()->back()->with('success', 'Leave request submitted successfully.');
+// // Redirect back to the form with a success message
+// return redirect()->back()->with('success', 'Leave request submitted successfully.');
 }
 
 
--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon;
 class Leave extends Model
 {
     use HasFactory;
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+   
+
+
 }
